9a191309f419720b6cf99cc0aaea8ec138fd8694
pankaj <pankajkalra09@gmail.com>
2024-03-18 11:35:46 -0400

    optimized

---
+++ src/main/java/UnifyedX/CustomerOnboarding/PageClasses/PageClass_Step2.java
@@ -0,0 +1,72 @@
+package UnifyedX.CustomerOnboarding.PageClasses;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import UnifyedX.CustomerOnboarding.ReusableComponenets.ReusableComponents;
+
+public class PageClass_Step2 extends ReusableComponents{
+
+	WebDriver driver;
+	
+	public PageClass_Step2(WebDriver driver) {
+		super(driver);// We are passing life of driver to parent class ReusableComponents as here driver is getting life from our testclass.
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//div/h1") WebElement Locator_Question1;
+	@FindBy(xpath="//button[@aria-label='Next Screen']") WebElement Locator_NextCTA_step2;
+	@FindBy(xpath="//div[@aria-label='animation']") WebElement Locator_Animation;
+	//@FindBy(xpath="//p[contains(text(), 'You can only select 3 options!')]") WebElement Locator_errormessage;
+	
+	//verify if question text is correct we want to see
+	public boolean Method_VerifyQuestionTest() {
+		String questiontext = Locator_Question1.getText();
+		boolean questiontxt = questiontext.contains("What do you need");
+		return questiontxt;
+		/*
+		 * if (questiontxt==true) {
+		 * System.out.println("Question text is showing up correctly as: "+questiontext)
+		 * ; } else { System.out.println("Question text is not correct."); }
+		 */
+	}
+	
+	
+	//Verify if Next CTA is disabled till the user has not selected 3 options.
+	public boolean Method_VerifyNextCTA_Beforeselection() {
+		boolean nextctabefore = Locator_NextCTA_step2.isEnabled();
+		return nextctabefore;
+		/*
+		 * if (nextcta==false) {
+		 * System.out.println("Next CTA is disabled initially without selection."); }
+		 */
+	}
+	
+	public boolean Method_VerifyNextCTA_Afterselection() {
+		boolean nextctaafter = Locator_NextCTA_step2.isEnabled();
+		return nextctaafter;
+		/*
+		 * if (nextcta==true) {
+		 * System.out.println("Next CTA is Clickable after 1 selections."); }
+		 */
+	}
+	
+	
+	//Verify if user select 1 values and error message is showing up.
+public void Method_SelectingFirstValues(String option5 ) throws Exception {
+		
+		List<WebElement> step1options = driver.findElements(By.xpath("//ul/li/button"));
+		WebElement optio3 = step1options.stream().filter(o->o.getText().equals(option5)).findFirst().orElse(null);
+		optio3.click();
+	}
+
+public void Method_VerifyNextCTA_Click() {
+	Locator_NextCTA_step2.click();
+}
+}
--- src/test/java/UnifyedX/CustomerOnboarding/TestOnboardingStep1.java
+++ src/test/java/UnifyedX/CustomerOnboarding/TestOnboardingStep1.java
@@ -8,58 +8,69 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
-import UnifyedX.CustomerOnboarding.PageClasses.ClassStep1;
+import UnifyedX.CustomerOnboarding.PageClasses.PageClass_Step1;
+import UnifyedX.CustomerOnboarding.PageClasses.PageClass_Step2;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestOnboardingStep1 {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-
+		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
-		driver.get("https://onboarding.qa.unifyedx.com/onboard");
-		Thread.sleep(2000);
+		driver.get("https://onboarding.qa.unifyedx.com/onboard");	
 		
-		ClassStep1 ObjectStep1 = new ClassStep1(driver);
+		PageClass_Step1 ObjectStep1 = new PageClass_Step1(driver);
 		ObjectStep1.Method_VerifyPageTitle();
+		String PageTitle = "Welcome to Unifyed";
+		Assert.assertEquals(ObjectStep1.Method_VerifyPageTitle(), PageTitle);
 		ObjectStep1.Method_VerifyUnifyedLogo();
+		Assert.assertTrue(true);
 		ObjectStep1.Method_VerifyUnifyedLogoClickable();
+		Assert.assertTrue(true);
 		ObjectStep1.Method_VerifyQuestionTest();
 		ObjectStep1.Method_VerifyAnimation();
+		Assert.assertTrue(true);
 		ObjectStep1.Method_VerifyNextCTA_Beforeselection();
+		Assert.assertFalse(false);
+		ObjectStep1.Method_GetOptionsSteps1();
 		String option1 = "Improving Annual Graduation Rates";
 		String option2 = "Increasing Quality of Applicants";
 		String option3 = "Growing Revenue from Tuition";
 		String option4 = "Increasing Student Engagement";
-		ObjectStep1.Method_SelectingThreeValues(option1, option2, option3);
+		String erromsg = "You can only select 3 options!";
+		ObjectStep1.Method_GetOptionsByName(option1, option2, option3);
 		ObjectStep1.Method_VerifyNextCTA_Afterselection();
+		Assert.assertTrue(true);
+		ObjectStep1.Method_Hardsleep();
 		ObjectStep1.Method_SelectingFourthValues(option4);
+		Assert.assertEquals(ObjectStep1.Method_VerifyErrorMessage(), erromsg);
+		ObjectStep1.Method_VerifyNextCTA_Click();
+		ObjectStep1.Method_Hardsleep();
 		
+		///////////////////////////////////////////////////////////////////
 		
-	
-		
-		
-		/*
-		 * if(driver.findElement(By.xpath("//a[@class=' cursor-default']/img")).
-		 * isEnabled()) {
-		 * //driver.findElement(By.xpath("//a[@class=' cursor-default']/img")).click();
-		 * System.out.println("logo is showing up"); }
-		 */
-		 
-		
-		//System.out.println("title of the question is: "+driver.findElement(By.xpath("//div/h1")).getText());
-		//System.out.println("Next CTA initially enabled: "+driver.findElement(By.xpath("//button[@type='Submit']")).isEnabled());		
-		
-		
-		//System.out.println(driver.findElement(By.xpath("//button[@type='Submit']")).isEnabled());
-		
-		//driver.findElement(By.xpath("//button[@type='Submit']")).click();
-		Thread.sleep(2000);
-		
+		PageClass_Step2 ObjectStep2 = new PageClass_Step2(driver);
+		ObjectStep2.Method_VerifyPageTitle();
+		Assert.assertEquals(ObjectStep1.Method_VerifyPageTitle(), PageTitle);
+		ObjectStep2.Method_VerifyUnifyedLogo();
+		ObjectStep2.Method_VerifyUnifyedLogoClickable();
+		ObjectStep2.Method_VerifyQuestionTest();
+		Assert.assertTrue(true);
+		ObjectStep2.Method_VerifyAnimation();
+		Assert.assertTrue(true);
+		ObjectStep2.Method_VerifyNextCTA_Beforeselection();
+		Assert.assertFalse(false);
+		String option5 = "User Friendly Experience";
+		ObjectStep2.Method_SelectingFirstValues(option5);
+		ObjectStep2.Method_VerifyNextCTA_Afterselection();
+		Assert.assertTrue(true);
+		ObjectStep2.Method_VerifyNextCTA_Click();
+		ObjectStep2.Method_Hardsleep();
 		driver.quit();
 	}
 
